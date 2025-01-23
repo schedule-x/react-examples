05704d13944163a7aa485ac43540fd9301b33452
Tom Österlund <tom_jonathan.oesterlund@prosiebensat1.com>
2025-01-23 06:27:42 +0100

    add draw example

---
+++ draw/src/App.tsx
@@ -0,0 +1,51 @@
+import {useState} from 'react'
+import './App.css'
+import {ScheduleXCalendar, useCalendarApp} from "@schedule-x/react";
+import {createDrawPlugin} from "@sx-premium/draw";
+import {createViewDay, createViewMonthGrid, createViewWeek} from "@schedule-x/calendar";
+
+import '@schedule-x/theme-default/dist/index.css'
+
+function App() {
+  const drawPlugin = useState(createDrawPlugin({
+    onFinishDrawing: (_event) => {
+      // send event to server
+    },
+
+    snapDuration: 30
+  }))[0];
+
+  const calendar = useCalendarApp({
+    views: [createViewDay(), createViewWeek(), createViewMonthGrid()],
+    plugins: [
+      drawPlugin
+    ],
+    callbacks: {
+      onMouseDownDateTime: (dateTime, mouseDownEvent) => {
+        drawPlugin.drawTimeGridEvent(dateTime, mouseDownEvent, {
+          title: 'New Event',
+        })
+      },
+
+      onMouseDownDateGridDate: (dateTime, mouseDownEvent) => {
+        drawPlugin.drawDateGridEvent(dateTime, mouseDownEvent, {
+          title: 'New Event',
+        })
+      },
+
+      onMouseDownMonthGridDate: (dateTime) => {
+        drawPlugin.drawMonthGridEvent(dateTime, {
+          title: 'New Event',
+        })
+      }
+    }
+  })
+
+  return (
+    <div>
+      <ScheduleXCalendar calendarApp={calendar} />
+    </div>
+  )
+}
+
+export default App
